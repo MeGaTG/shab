c912451f5719c5b6fb36f8231c63ff1410ee7678
This_Is_Amir <arabiamir13@gmail.com>
2016-03-23 08:16:58 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {152321430},
     admin_users = {},
     disabled_channels = {}
   }
